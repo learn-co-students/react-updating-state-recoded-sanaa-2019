4d39f32d87d2ddff1ff61518f346ab8ae1c48255
Sarah Alafif <sar1997yah@gmail.com>
2019-12-17 18:59:56 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import ClickityClick from './components/ClickityClick';
 ReactDOM.render(
   <div>
-    Mount Components Here
+  </
   </div>,
   document.getElementById('root')
 );
