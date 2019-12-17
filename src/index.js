bc4560b9a1b98f5b2edf56d8e4697f84ba8bb9fa
Sarah Alafif <sar1997yah@gmail.com>
2019-12-17 19:00:06 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import ClickityClick from './components/ClickityClick';
 ReactDOM.render(
   <div>
-  </
+ <ClickityClick />
   </div>,
   document.getElementById('root')
 );
